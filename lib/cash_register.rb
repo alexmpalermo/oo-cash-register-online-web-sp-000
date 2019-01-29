38be9d216bb82e17f06a67b4be73400a9b500841
Alexandra Palermo <alexmpalermo@gmail.com>
2019-01-29 21:04:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,17 @@
 class CashRegister
-  attr_accessor :total 
+  attr_accessor :total, :price, :title, :person, :employee
   
  
   
-  def initialization(total)
-  @total = 0  
-  
+  def initialization(total = 0, person)
+  @total = total  
+  if @person == @employee
+    self.apply_discount
 end 
+
+def add_item(price, title)
+  @total 
+end
 
 def apply_discount
   
