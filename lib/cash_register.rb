08a0032776ccd56c819c720dd885dd426d7087c6
Alexandra Palermo <alexmpalermo@gmail.com>
2019-01-29 20:50:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
+  attr_accessor :total 
+  
+ 
+  
+  def initialization(total)
+  @total = 0 
+  
+end 
 
+def apply_discount
+  
+end 
+  
+  def void_last_transaction
+    
+  end 
+  
+  
+  
+  
+  
+end
