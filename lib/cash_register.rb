7eb42f6c8ebca3b8478c6af4d133e3c43310d0a7
Alexandra Palermo <alexmpalermo@gmail.com>
2019-01-29 21:51:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -32,8 +32,10 @@ def initialize (employee_discount = nil)
   end
  
  
-  def void_last_transaction()
-    @total = @total - @items[-1]
+  def void_last_transaction
+    
+    @total = @total - 
+    @items[-1]
   end
   
   
